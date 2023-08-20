871c6601dedecb8bbed7140538f2b03f335bec59
Long Do <ldo@corpus.cz>
2023-08-21 00:59:59 +0200

    přejmenování

---
+++ jalovice_a_jalovce.py
@@ -0,0 +1,11 @@
+"""
+jalovice_a_jalovce.py: druhý projekt do Engeto Online Python Akademie
+autor: Long Do
+e-mail: long.do@email.cz
+discord: Long Do#7160
+"""
+
+from spusteni_hry import spustit_hru
+
+if __name__ == "__main__":
+    spustit_hru()
