68755dac2869b984aac37eb6be41cc0264da9a62
Long Do <ldo@corpus.cz>
2023-08-20 18:00:25 +0200

    uprava podmínky

--- herni_funkce.py
+++ herni_funkce.py
@@ -11,6 +11,10 @@ def vygenerovat_cislo(pocetCislic: int) -> int:
   return vygenerovaneCislo
 
 def validovat_cislo(hadane_cislo: any) -> bool:
+    if hadane_cislo is "":
+      print ("Nebylo nic doplněno, zkus to ještě jednou.")
+      return False
+    
     if not hadane_cislo.isnumeric():
       print("Nejedná se o číslo.")
       return False
@@ -21,11 +25,11 @@ def validovat_cislo(hadane_cislo: any) -> bool:
       print("Člověče, číslo nemůže začínat nulou.")
       return False
 
-    if len(hadane_cislo) > 4:
+    if len(hadane_cislo) < 4:
       print( "A jé, musí se jednat o čtyřmístní číslo. Přidej číslice.")
       return False
     
-    if len(hadane_cislo) < 4:
+    if len(hadane_cislo) > 4:
       print("Musí se jednat o čtyřmístní číslo. Uber číslice. Bože!")
       return False
     
